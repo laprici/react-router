140bff5564b4d66ba1bdbecec41f56b4a4bb60dd
laprici <pricilabadilla@gmail.com>
2025-04-29 21:03:05 -0400

    feat: agrega layout y diseño chat

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import { AuthLayout } from "./auth/layout/AuthLayout";
+// import { AuthLayout } from "./auth/layout/AuthLayout";
+
+import ChatLayout from "./chat/layout/ChatLayout";
+import ChatPage from "./chat/pages/ChatPage";
 
 function App() {
   return (
     <>
-      <AuthLayout />
+      {/* <AuthLayout /> */}
+      <ChatLayout>
+        <ChatPage />
+      </ChatLayout>
     </>
   );
 }
